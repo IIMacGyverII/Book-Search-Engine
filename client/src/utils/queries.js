98abf54c18595d74b5a71db4d7820ef817627865
Joshua Christianson <macgyverjunk@yahoo.com>
2022-06-02 16:12:00 -0500

    can hit all endpoints in ql and client queries.js should be ready

---
+++ client/src/utils/queries.js
@@ -0,0 +1,21 @@
+// imported from act 26 unit-21
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;

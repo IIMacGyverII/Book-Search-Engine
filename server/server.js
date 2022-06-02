9f220cedced660885227dffbe19317616cc1df00
Joshua Christianson <macgyverjunk@yahoo.com>
2022-06-02 15:31:21 -0500

    imported server.js from act26

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
+// imported from act 26 unit-21
 const express = require("express");
 const path = require("path");
 const {ApolloServer} = require("apollo-server-express");
 const {authMiddleware} = require("./utils/auth");
 const {typeDefs, resolvers} = require("./schemas");
 const db = require("./config/connection");
-// const routes = require('./routes');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -17,7 +18,7 @@ const server = new ApolloServer({
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
@@ -26,8 +27,8 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-// app.use(routes);
 
+// Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({app});

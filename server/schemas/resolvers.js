541fb40533cd54f3ddcb7b6678a087d96da06614
Joshua Christianson <macgyverjunk@yahoo.com>
2022-06-02 19:34:08 -0500

    all is working after a wild goose chase.  Clear local storage next time!

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,60 +4,69 @@ const {User} = require('../models');
 const {signToken} = require('../utils/auth');
 
 const resolvers = {
-  Query: {
-      users: async (parent) => {
-          return User.find();
+    Query: {
+        user: async (parent, {userId}) => {
+            return User.findOne({_id: userId});
         },
-      user: async (parent, {userId}) => {
-          return User.findOne({_id: userId});
+        users: async (parent) => {
+            return User.find();
+        },
     },
-  },
-  Mutation: {
-    addUser: async (parent, {username, email, password}) => {
-        const user = await User.create({username, email, password});
-        const token = signToken(user);
-        return {token, user};
+
+    Mutation: {
+        addUser: async (parent, {username, email, password}) => {
+            const user = await User.create({username, email, password});
+            const token = signToken(user);
+            return {token, user};
+        },
+        login: async (parent, {email, password}) => {
+            const user = await User.findOne({email});
+
+            if (!user) {
+                throw new AuthenticationError("No user found with this email address");
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw){
+                throw new AuthenticationError("Incorrect credentials");
+            }
+
+            const token = signToken(user);
+
+            return {token, user};
+        },
+        saveBook: async (
+            parent, 
+            {userId, authors, description, bookId, image, link, title}
+        ) => {
+            return User.findOneAndUpdate(
+                {_id: userId},
+                {
+                    $addToSet: {
+                        savedBooks: {authors, description, bookId, image, link, title},
+                    },
+                },
+                {
+                    new: true, 
+                    runValidators: true,
+                }
+            );
+        },
+        removeBook: async (parent, {userId, bookId}) => {
+            return User.findOneAndUpdate(
+                {_id: userId},
+                {
+                    $pull : {
+                        savedBooks: {
+                            bookId: bookId,
+                        },
+                    },
+                },
+                {new: true}
+            );
+        },
     },
-    login: async (parent, {email, password}) => {
-        const user = await User.findOne({email});
-
-        if (!user) {
-            throw new AuthenticationError("No user found with this email address");
-        }
-
-        const correctPw = await user.isCorrectPassword(password);
-
-        if (!correctPw){
-            throw new AuthenticationError("Incorrect credentials");
-        }
-
-        const token = signToken(user);
-
-        return {token, user};
-    },
-    saveBook: async (parent, { newBook }, context) => {
-      if (context.user) {
-        const updatedUser = await User.findByIdAndUpdate(
-          { _id: context.user._id },
-          { $push: { savedBooks: newBook }},
-          { new: true }
-        );
-        return updatedUser;
-      }
-      throw new AuthenticationError('You need to be logged in!');
-    },
-    removeBook: async (parent, { bookId }, context) => {
-      if (context.user) {
-        const updatedUser = await User.findByIdAndUpdate(
-          { _id: context.user._id },
-          { $pull: { savedBooks: { bookId }}},
-          { new: true }
-        );
-        return updatedUser;
-      }
-      throw new AuthenticationError('You need to be logged in!');
-    },
-  },
 };
 
 module.exports = resolvers;
